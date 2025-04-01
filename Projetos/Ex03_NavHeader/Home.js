48bdf50fa13687db9d7c3a035a41838633256b0a
ObeliscoAtormentador1 <tamosdejesusjoaovitor@gmail.com>
2025-03-31 21:50:46 -0300

    Atividade3aula12

---
+++ Projetos/Ex03_NavHeader/Home.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, Button, StatusBar } from "react-native";
+import styles from "./styles";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+type Props = NativeStackScreenProps<RoutesParams, "Home">;
+
+export default function Home({ navigation }: Props) {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      {/* Botão que navega para a tela Details, passando parâmetros 'title', 'content' e 'stock'. */}
+      <Button
+        title="First Item"
+        onPress={() => {
+          navigation.navigate("Details", {
+            title: "First Item",
+            content: "First Item Content",
+            stock: 0,
+          });
+        }}
+      />
+      <Button
+        title="Second Item"
+        onPress={() => {
+          navigation.navigate("Details", {
+            title: "Second Item",
+            content: "Second Item Content",
+            stock: 0,
+          });
+        }}
+      />
+      {/* Botão que navega para a tela Details, passando parâmetros 'title', 'content' e 'stock'. */}
+      <Button
+        title="Third Item"
+        onPress={() => {
+          navigation.navigate("Details", {
+            title: "Third Item",
+            content: "Third Item Content",
+            stock: 200,
+          });
+        }}
+      />
+    </View>
+  );
+}
